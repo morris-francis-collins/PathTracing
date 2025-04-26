b57a2ac0dcdc1da1a6782b91a99f91e4b9d54478
morris-francis-collins <morris.francis.collins@gmail.com>
2025-04-25 19:58:37 -0700

    Initial Commit

---
+++ PathTracing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PathTracing
+//
+//  Created by Moritz Kohlenz on 4/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PathTracing/PathTracingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathTracingApp.swift
+//  PathTracing
+//
+//  Created by Moritz Kohlenz on 4/25/25.
+//
+
+import SwiftUI
+
+@main
+struct PathTracingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
